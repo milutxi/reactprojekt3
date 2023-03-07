4cc356ef559dfd4eaf2035f2e75ee2ccbe921309
lutxi <aliaga_sheila@hotmail.com>
2023-03-07 10:31:22 +0100

    uppdating

--- projekt3/src/App.jsx
+++ projekt3/src/App.jsx
@@ -1,32 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
+import { parseISO, intervalToDuration } from 'date-fns'
 import './App.css'
 
+import snowman from './pictures/snowman.jpg'
+import skiing from './pictures/skiing.jpg'
+import winter from './pictures/winter.jpg'
+import moresnow from './pictures/moresnow.jpg'
+import snows from './pictures/snows.jpg'
+import night from './pictures/night.jpg'
+import winterplane from './pictures/winterPlane.jpg'
+import plane from './pictures/planeToSummer.jpg'
+import wintermore from './pictures/wintermore.jpg'
+import sunflowers from './pictures/sunflowers.jpg'
+import sunsand from './pictures/sunsand.jpg'
+import summer from './pictures/summer.jpg'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [
+    toSummer, 
+    setToSummer
+  ] = useState({})
+  
+  useEffect(()=> {
+    const intervalId =setInterval(() => {
+      const summer = parseISO('2023-06-21T00:00:00')
+      const interval = intervalToDuration( {
+        start: new Date(), 
+        end: summer
+      })
+    setToSummer(interval)
+    }, 1000)
+
+    return() => {
+      clearInterval(intervalId)
+    }
+   }, [])
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="column1">
+        <section className="S1A">
+        <img className='snowman' src={snowman} alt='snowman' />
+        <p>I HOPPE TO GET FROM WINTER TO SUMMER IN NO TIME AND NOT TO DIE TRYING NOT FREZZE TOO MUCH. I LOVE THE WARM WEATHER.</p>
+        <div className='subgroup'>
+        <img className='skiing' src={skiing} alt='person skiing' />
+        <img className='winter' src={winter} alt='view of winter with snow' />
+        </div>
+        <div>
+        <img className='moresnow' src={moresnow} alt='view of forest with snow' />
+        </div>
+        <div className='subgroup2'>
+        <img className='snows' src={snows} alt='person walking at night while snowing' />
+        <img className='night' src={night} alt='night in winter' />
+        </div>
+        </section>
+        <section className="S1B">
+        <img className='winterplane' src={winterplane} alt='plane in winter' />
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="column2">
+        <section className="S2A">
+          <img className='plane' src={plane} alt='plane to summer' />
+        </section>
+        <section className="S2B">
+          <h1 className='days'>{toSummer.months} months, {toSummer.days} days, {toSummer.hours}:{toSummer.minutes}:{toSummer.seconds}</h1>
+          <h1 className='title'>On the Way to Summer!!!</h1>
+        </section>
+        <section className="S2C">
+        <img className='wintermore' src={wintermore} alt='winter forest with snow' />
+        </section>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="column3">
+        <section className="S3A">
+        <img className='sunflowers' src={sunflowers} alt='sunflowers field' />
+        </section>
+        <section className="S3B">
+          <img className='sunsand' src={sunsand} alt='sun painted in the sand beach' />
+        </section>
+        <section className="S3C">
+          <img className='summer' src={summer} alt='flowers field in summer' />
+        </section>
+      </div>
     </div>
   )
 }
